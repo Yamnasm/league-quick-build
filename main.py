16e42a64105c3a28be66e26aa78577200060b967
Yamnasm <jrts220@gmail.com>
2022-04-10 17:42:34 +0100

    functionally complete

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
-def champion_selection(champ):
-    return f"https://www.metasrc.com/aram/champion/{champ}"
+import requests
+from bs4 import BeautifulSoup
 
+class Champion:
+    def __init__(self, champ):
+        self.name = champ
+        self.page = self._get_champ_page()
+        self.truename = self._get_true_name()
+        self.items = self._retrieve_items()
+
+    def _get_champ_page(self):
+        URL = f"https://www.metasrc.com/aram/champion/{self.name}"
+        page = requests.get(URL)
+        soop = BeautifulSoup(page.content, "html.parser")
+        return soop
+
+    def _get_true_name(self):
+        true_name_search = self.page.find("h2", class_="_fcukao")
+        r1 = true_name_search.text.replace("Best ", "")
+        r2 = r1.replace(" Summoner Spells", "")
+        return r2
+
+    def _retrieve_items(self):
+        soop = self.page
+
+        item_build_title = soop.find("h2", class_="_fcukao", text=f"Best {self.truename} Item Build")
+        build_items = item_build_title.parent.find_all("div")
+        champ_items = []
+        for obj in build_items:
+            result = obj.find_all("img", class_="lozad")
+            if result:
+                champ_items.append(result[0]["alt"])
+        return list(dict.fromkeys(champ_items))
+
+def print_items(champion):
+    print(f"   Items:")
+    for item in champion.items[0:-1]:
+        print(f"....... > {item}")
+    print(f" Elixir > {champion.items[-1]}")
 
 def main():
-    print(champion_selection("aatrox"))
+    champion = Champion(input("Champion: "))
+    print_items(champion)
 
 if __name__ == "__main__":
     main()
